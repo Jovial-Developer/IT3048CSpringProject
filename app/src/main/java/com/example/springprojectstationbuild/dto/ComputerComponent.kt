ecfbe9792ddfa8bf937e96eb1d63095e2671d1e7
James <48566053+Jovial-Developer@users.noreply.github.com>
2022-02-27 16:41:43 -0500

    Updated DTO to have more fields to show the user results in component list

--- app/src/main/java/com/example/springprojectstationbuild/dto/ComputerComponent.kt
+++ app/src/main/java/com/example/springprojectstationbuild/dto/ComputerComponent.kt
@@ -2,9 +2,13 @@ package com.example.springprojectstationbuild.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class ComputerComponent(@SerializedName("Price") var price : Int, @SerializedName("Name") var name: String){
-    private var component = name + " " + price
-    override fun toString(): String {
+//ID is generated when new object is created in firebase
+//index is order in firebase object list
+//Boolean hide is info on whether the user has hidden the object in the list of component objects
+//Boolean addtocart is whether the user wants to add the object in their list of computer components to buy.
+data class ComputerComponent(@SerializedName("ID") val id : Int, @SerializedName("index") var index : Int, @SerializedName("Price") var price : Int, @SerializedName("Name") var name: String, @SerializedName("Brand") var brand: String, @SerializedName("Model") var model: String, @SerializedName("Hide") var hide: Boolean, @SerializedName("AddToCart") var addtocart: Boolean){
+    private var component = name + " " + id + " " + index + " " + price + " " + brand + " " + model
+            override fun toString(): String {
         return component
     }
 }
