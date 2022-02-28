ce0c633cbfe092ea8a8a2576617153cc7d3f9d68
chamlamh <70345325+chamlamh@users.noreply.github.com>
2022-02-27 21:10:59 -0500

    Create Main DTO and activity class

---
+++ app/src/main/java/com/example/springprojectstationbuild/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.springprojectstationbuild
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(){
+
+    // Will be used to code the main page of the Application.
+}
