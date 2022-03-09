32e3d42855520ba212afe508b3d1808ec1d566c6
healeyke <healeyke@mail.uc.edu>
2022-03-09 14:07:45 -0500

    added Retrofit and Dispatchers imports

--- app/src/main/java/com/example/springprojectstationbuild/service/ComponentService.kt
+++ app/src/main/java/com/example/springprojectstationbuild/service/ComponentService.kt
@@ -1,7 +1,9 @@
-package com.example.springprojectstationbuild.service/*package com.example.springprojectstationbuild.service
+package com.example.springprojectstationbuild.service
 
 import com.example.springprojectstationbuild.dao.IComponentDAO
 import com.example.springprojectstationbuild.dto.ComputerComponent
+import com.example.springprojectstationbuild.RetrofitClientInstance
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
@@ -13,12 +15,11 @@ interface IComponentService {
 class ComponentService : IComponentService {
     override suspend fun fetchComputerComponents(): List<ComputerComponent>? {
         return withContext(Dispatchers.IO){
-            val service = retrofitClientInstance.retrofitInstance?.create(IComponentDAO::class.java)
+            val service = RetrofitClientInstance.retrofitInstance?.create(IComponentDAO::class.java)
             val components = async {service?.getAllComponents()}
             var result = components.await()?.awaitResponse()?.body()
             return@withContext result
         }
     }
 }
-*/
 
