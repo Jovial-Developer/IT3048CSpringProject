73269f623fd267238bf06e3a0c6c0d59e6ff9837
kylerseverance <67241123+kylerseverance@users.noreply.github.com>
2022-03-09 21:32:50 -0500

    I created a MainViewModel for the application.

---
+++ app/src/main/java/com/example/springprojectstationbuild/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.springprojectstationbuild
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.springprojectstationbuild.dto.ComputerComponent
+import com.example.springprojectstationbuild.service.ComponentService
+import kotlinx.coroutines.launch
+
+
+    class MainViewModel : ViewModel() {
+
+
+        var components : MutableLiveData<List<ComputerComponent>> = MutableLiveData<List<ComputerComponent>>()
+        var componentService : ComponentService = ComponentService()
+
+
+        fun fetchComponents() {
+            viewModelScope.launch{
+                var innerComponents = componentService.fetchComputerComponents()
+                components.postValue(innerComponents)
+            }
+        }
+    }
