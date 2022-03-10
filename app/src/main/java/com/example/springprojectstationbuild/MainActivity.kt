f9273fbe08180337c8c58268f55dc0eec266079d
kylerseverance <67241123+kylerseverance@users.noreply.github.com>
2022-03-09 23:22:16 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/springprojectstationbuild/MainActivity.kt
+++ app/src/main/java/com/example/springprojectstationbuild/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.springprojectstationbuild
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.LocalContext
 
 
 
@@ -20,5 +25,36 @@ class MainActivity : AppCompatActivity() {
             R.layout.settings_activity
 
         }
+    }
+}
+@Composable
+fun mainButtons() {
+
+    val context = LocalContext.current
+    val text = "You clicked this button"
+    val duration = Toast.LENGTH_LONG
+
+    Column {
+
+        Button(
+            onClick = {
+                Toast.makeText(context, text, duration).show()
+            }
+        ) { Text(text = "Select Existing PC") }
+        Button(
+            onClick = {
+                Toast.makeText(context, text, duration).show()
+            }
+        ) { Text(text = "Add a new PC") }
+        Button(
+            onClick = {
+                Toast.makeText(context, text, duration).show()
+            }
+        ) { Text(text = "Login to My Account") }
+        Button(
+            onClick = {
+                Toast.makeText(context, text, duration).show()
+            }
+        ) { Text(text = "Create Account") }
     }
 }
