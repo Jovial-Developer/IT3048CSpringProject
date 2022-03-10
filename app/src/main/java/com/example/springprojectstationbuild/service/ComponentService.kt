7d2beab950768b58df82a63ebeb59a773b1e2191
kylerseverance <67241123+kylerseverance@users.noreply.github.com>
2022-03-09 22:22:24 -0500

    I capitalized the "R" in RetrofitClientInstance to match the object name I created, and in order to comply with Kotlin Coding convention, "Names of classes and objects start with an uppercase letter and use camel case."

--- app/src/main/java/com/example/springprojectstationbuild/service/ComponentService.kt
+++ app/src/main/java/com/example/springprojectstationbuild/service/ComponentService.kt
@@ -1,10 +1,11 @@
 package com.example.springprojectstationbuild.service
-
+import com.example.springprojectstationbuild.RetrofitClientInstance
 import com.example.springprojectstationbuild.dao.IComponentDAO
 import com.example.springprojectstationbuild.dto.ComputerComponent
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.Dispatchers
+import retrofit2.awaitResponse
 
 interface IComponentService {
     suspend fun fetchComputerComponents() : List<ComputerComponent>?
@@ -13,7 +14,7 @@ interface IComponentService {
 class ComponentService : IComponentService {
     override suspend fun fetchComputerComponents(): List<ComputerComponent>? {
         return withContext(Dispatchers.IO){
-            val service = retrofitClientInstance.retrofitInstance?.create(IComponentDAO::class.java)
+            val service = RetrofitClientInstance.retrofitInstance?.create(IComponentDAO::class.java)
             val components = async {service?.getAllComponents()}
             var result = components.await()?.awaitResponse()?.body()
             return@withContext result
