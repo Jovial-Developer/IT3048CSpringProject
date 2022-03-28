670934474becdcf2af957ab812c8658fcbc67d3e
ishidasb <ishidasb@mail.uc.edu>
2022-03-28 12:05:35 -0400

    figureing how to import chexboxs to SettingsActivity.kt

--- app/src/main/java/com/example/springprojectstationbuild/SettingsActivity.kt
+++ app/src/main/java/com/example/springprojectstationbuild/SettingsActivity.kt
@@ -1,14 +1,23 @@
 package com.example.springprojectstationbuild
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+
+
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_main);
+        // Finding CheckBox by its unique ID
+        checkboxCPU = (CheckBox) findViewById (R.id.cbCPU);
+        checkboxpower=(CheckBox)findViewById(R.id.cbpowersource);
+        checkboxWIFI=(CheckBox)findViewById(R.id.cbwificard);
+        checkboxRAM=(CheckBox)findViewById(R.id.cbRAM);
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
@@ -16,6 +25,19 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        btnSearch.setOnClickListener
+        {
+            val checkboxCPU = cbCPU.isChecked
+            val checkboxWIFI = cbwificard.isChecked
+            if (checkboxCPU.isChecked)
+            {
+                //code to go to next page
+            }
+
+
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
