61bb460a91d635c6318e784f104377e404fbfa99
chamlamh <70345325+chamlamh@users.noreply.github.com>
2022-03-29 14:13:20 -0400

    Finishing up DTO

--- app/src/main/java/com/example/springprojectstationbuild/dto/HardDrive.kt
+++ app/src/main/java/com/example/springprojectstationbuild/dto/HardDrive.kt
@@ -1,5 +1,5 @@
 package com.example.springprojectstationbuild.dto
 
-data class HardDrive(var name: String = "", var rating: String = "", var pride_usd: Double = 0.00) {
+data class HardDrive(var name: String = "", var rating: String = "", var pride_usd: Double = 0.00, var capacity: String = "",
+var type: String = "", var interfaces: String = "") {
 }
-
--- app/src/main/java/com/example/springprojectstationbuild/dto/PowerSupply.kt
+++ app/src/main/java/com/example/springprojectstationbuild/dto/PowerSupply.kt
@@ -1,4 +1,5 @@
 package com.example.springprojectstationbuild.dto
 
-data class PowerSupply(var name: String = "", var rating: String = "", var pride_usd: Double = 0.00,) {
+data class PowerSupply(var name: String = "", var rating: String = "", var pride_usd: Double = 0.00, var form_factor: String = "",
+var effciency_rating: String =  "", var wattage: String = "", var color: String = "") {
 }
