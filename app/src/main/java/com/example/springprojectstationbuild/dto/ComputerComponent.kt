718b6b120460b162efce0cf0e317d45fb2245729
chamlamh <70345325+chamlamh@users.noreply.github.com>
2022-03-29 13:55:02 -0400

    Adding Dto for case, cpu, gpu

--- app/src/main/java/com/example/springprojectstationbuild/dto/ComputerComponent.kt
+++ app/src/main/java/com/example/springprojectstationbuild/dto/ComputerComponent.kt
@@ -3,7 +3,7 @@ package com.example.springprojectstationbuild.dto
 import com.google.gson.annotations.SerializedName
 
 data class ComputerComponent(@SerializedName("Price") var price : Int, @SerializedName("Name") var name: String){
-    private var component = name + " " + price
+    private var component = name + "" + price
     override fun toString(): String {
         return component
     }
