4d6ed58ae3c197277cd6383258b51cee5b78f5ce
YifeiYang20 <yang3yf@mail.uc.edu>
2022-04-05 18:07:36 -0400

    Yifei Yang (yang3yf) code review 2

--- app/src/main/java/com/example/springprojectstationbuild/SettingsActivity.kt
+++ app/src/main/java/com/example/springprojectstationbuild/SettingsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.springprojectstationbuild
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import androidx.preference.CheckBoxPreference
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,17 +15,58 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager
+            this.supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
+                .replace(R.id.settings, SettingsFragmentEo())
                 .commit()
+        } else {
+            throw Exception("This value already exists, please enter a valid value. ")
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragmentEo : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
     }
+
+    abstract class SettingsFragment : PreferenceFragmentCompat(),
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+        private fun setPreferenceSummary(preference: Preference, value: Any?) {
+            val stringValue = value?.toString()
+
+            if (preference is ListPreference) {
+                val prefIndex = preference.findIndexOfValue(stringValue ?: "")
+                if (prefIndex >= 0) {
+                    preference.setSummary(preference.entries[prefIndex])
+                }
+            } else {
+                preference.summary = stringValue
+            }
+        }
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+            addPreferencesFromResource(R.xml.root_preferences)
+
+            val sharedPreferences = preferenceScreen.sharedPreferences
+            val prefScreen = preferenceScreen
+            val count = prefScreen.preferenceCount
+            for (i in 0 until count) {
+                val p = prefScreen.getPreference(i)
+                if (p !is CheckBoxPreference) {
+                    val value = sharedPreferences?.getString(p.key, "")
+                    setPreferenceSummary(p, value)
+                }
+            }
+        }
+    }
+}
+
+
+
+private fun Any.replace(settings: Int, settingsFragment: SettingsActivity.SettingsFragment) {
+
 }
--- app/src/main/java/com/example/springprojectstationbuild/dto/ComputerComponent.kt
+++ app/src/main/java/com/example/springprojectstationbuild/dto/ComputerComponent.kt
@@ -6,7 +6,16 @@ import com.google.gson.annotations.SerializedName
 //index is order in firebase object list
 //Boolean hide is info on whether the user has hidden the object in the list of component objects
 //Boolean addtocart is whether the user wants to add the object in their list of computer components to buy.
-data class ComputerComponent(@SerializedName("ID") val id : Int, @SerializedName("index") var index : Int, @SerializedName("Price") var price : Int, @SerializedName("Name") var name: String, @SerializedName("Brand") var brand: String, @SerializedName("Model") var model: String, @SerializedName("Hide") var hide: Boolean, @SerializedName("AddToCart") var addtocart: Boolean){
+data class ComputerComponent(
+    @SerializedName("ID") val id : Int,
+    @SerializedName("index") var index : Int,
+    @SerializedName("Price") var price : Int,
+    @SerializedName("Name") var name: String,
+    @SerializedName("Brand") var brand: String,
+    @SerializedName("Model") var model: String,
+    @SerializedName("Hide") var hide: Boolean,
+    @SerializedName("AddToCart") var addtocart: Boolean
+    ){
     private var component = name + " " + id + " " + index + " " + price + " " + brand + " " + model
             override fun toString(): String {
         return component
