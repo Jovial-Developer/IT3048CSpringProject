4ceebc78b3cbd88f7f47ab7e1e0e00a0ac4a0803
chamlamh <70345325+chamlamh@users.noreply.github.com>
2022-04-19 03:42:44 -0400

    Adding working link to Json

--- app/src/main/java/com/example/springprojectstationbuild/dao/IComponentDAO.kt
+++ app/src/main/java/com/example/springprojectstationbuild/dao/IComponentDAO.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 interface IComponentDAO {
 
-    @GET("/docyx/pc-part-dataset/main/data/raw")
+    @GET("b/625e67abbc312b30ebe93989")
     fun getAllComponents() : Call<ArrayList<ComputerComponent>>
 
 }
